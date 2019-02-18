859480287b28bc6a902a3ba9402c2ff61f39d66d
Jérémy <brunel.jeremy9@gmail.com>
2019-02-18 19:38:26 +0100

    JSON added

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home()
+    {
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'HomeController',
+            'title' => 'Gestionnaire de cartes Yu-Gi-Oh'
+        ]);
+    }
+}
